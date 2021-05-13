ba96dc94639850bf5e74c7f51d1a6d2ff336d0d1
Emma Mcmillin <emma.mcmillin@zhdk.ch>
2021-05-13 16:30:42 +0200

    camera controller

--- Assets/05_Scripts/CameraController.cs
+++ Assets/05_Scripts/CameraController.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+   
+    
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
+    private Vector3 cameraPos;
+    public float panlimitX;
+    public float panlimitZ;
+    public float scrollSpeed = 20f;
+    public float minY;
+    public float maxY;
     
+
+    private void Start()
+    {
+        cameraPos = transform.position;
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -25,9 +39,18 @@ public class CameraController : MonoBehaviour
         }
         if (Input.GetKey("a") || Input.mousePosition.x <=  panBorderThickness)
         {
+           
             pos.x -= panSpeed * Time.deltaTime;
         }
 
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y += scroll * scrollSpeed * 100f * Time.deltaTime;
+        
+        pos.x = Mathf.Clamp(pos.x, (cameraPos.x - panlimitX), (cameraPos.x + panlimitX));
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, (cameraPos.z - panlimitZ), (cameraPos.z + panlimitZ));
+
+       
         transform.position = pos;
     }
 }
