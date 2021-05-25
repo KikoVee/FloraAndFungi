a9162cf2137cd90d638fd45a4329fa50860ba864
Emma Mcmillin <emma.mcmillin@zhdk.ch>
2021-05-25 09:17:13 +0200

    adding tutorial sequence

--- Assets/05_Scripts/Tutorial.cs
+++ Assets/05_Scripts/Tutorial.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
+    [SerializeField] public GameObject[] tutorialText;
+    private GameObject text;
+    private Text oldText;
+    private int textNumber = 0;
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject playButton;
+
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = tutorialText[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+         
+        
+    }
+
+    public void TutorialText()
+    {
+        if (textNumber <= tutorialText.Length)
+        {
+            GameObject oldText = text;
+            textNumber += 1;
+            text = tutorialText[textNumber];
+            text.SetActive(true); 
+            oldText.SetActive(false);  
+        }
+        else
+        {
+            continueButton.SetActive(false);
+            playButton.SetActive(true);
+        }
         
     }
 }
