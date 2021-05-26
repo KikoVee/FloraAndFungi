cd9c9e333e41f67becf1263c995807c8bcf8bead
Emma Mcmillin <emma.mcmillin@zhdk.ch>
2021-05-26 11:15:36 +0200

    text renders on canvas now

--- Assets/05_Scripts/UI/DisplayUI.cs
+++ Assets/05_Scripts/UI/DisplayUI.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 public class DisplayUI : MonoBehaviour
 {
     public string myString;
-    public Text myText;
+    public Text textPrefab;
+    private Text myText;
     public float fadeTime;
     public bool displayInfo;
+    private Camera camera;
+    private Canvas myCanvas;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCanvas = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<Canvas>();
+        camera = FindObjectOfType<Camera>();
+        myText = Instantiate(textPrefab, myCanvas.transform).GetComponent<Text>();
         myText.color = Color.clear;
     }
 
     // Update is called once per frame
     void Update()
     {
+        myText.transform.position = camera.WorldToScreenPoint(transform.position);
         FadeText();
     }
 
