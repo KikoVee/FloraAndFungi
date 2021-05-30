520f52f5c4a9985bfdb562a99b6de0d23846a9a5
Emma Mcmillin <emma.mcmillin@zhdk.ch>
2021-05-30 13:24:10 +0200

    UI Buttons set up

---
+++ Assets/05_Scripts/UI/UIHover.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    public GameObject textPrefab;
+    private Text myText;
+    private Image myImage;
+    [SerializeField] private Color myImageColor;
+    [SerializeField] private Color myTextColor;
+
+    public float fadeTime;
+    public bool displayInfo;
+    
+    
+    void Start()
+    {
+        myImage = textPrefab.GetComponentInChildren<Image>();
+        myImage.color = Color.clear;
+        myText = textPrefab.GetComponentInChildren<Text>();
+        myText.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("The cursor entered the selectable UI element.");
+        displayInfo = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        displayInfo = false;
+
+    }
+
+    
+
+    private void FadeText()
+    {
+        if (displayInfo)
+        {
+            myImage.color = Color.Lerp(myImage.color, myImageColor, fadeTime * Time.deltaTime);
+            myText.color = Color.Lerp(myText.color, myTextColor, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+            myImage.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+        
+    }
+}
