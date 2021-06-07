353ba424f1d3f755dfa00da1906e2b5dcdbb5051
Emma Mcmillin <emma.mcmillin@zhdk.ch>
2021-06-07 15:10:11 +0200

    fixed lighting

--- Assets/05_Scripts/CameraController.cs
+++ Assets/05_Scripts/CameraController.cs
@@ -7,9 +7,11 @@ public class CameraController : MonoBehaviour
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
     private Vector3 cameraPos;
-    private Quaternion cameraRot;
+    private float cameraRot;
     public float panlimitX;
     public float panlimitZ;
+    public float rotlimitMin;
+    public float rotlimitMax;
     public float scrollSpeed = 20f;
     public float minY;
     public float maxY;
@@ -20,7 +22,7 @@ public class CameraController : MonoBehaviour
     private void Start()
     {
         cameraPos = transform.position;
-        cameraRot = transform.rotation;
+        cameraRot = transform.rotation.y;
     }
 
 
@@ -29,6 +31,7 @@ public class CameraController : MonoBehaviour
     {
         Vector3 pos = transform.position;
         float rotationY = transform.rotation.y;
+
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
@@ -48,16 +51,26 @@ public class CameraController : MonoBehaviour
             pos.x -= panSpeed * Time.deltaTime;
         }
 
-        /*if (Input.GetKey("q"))
+        /*if (Input.GetKey("q") || Input.GetKey("e"))
         {
-            rotationY += panSpeed * Time.deltaTime;
-            rotationObject.transform.Rotate(0, rotationY, 0);        
-        }
-        if (Input.GetKey("e"))
-        {
-            rotationY -= panSpeed * Time.deltaTime;
-            rotationObject.transform.Rotate(0, rotationY, 0);
+            if (Input.GetKey("q"))
+            {
+                rotationY -= panSpeed * Time.deltaTime;
+                rotationObject.transform.Rotate(0, rotationY, 0);
+                rotationY = Mathf.Clamp(rotationY, (cameraRot - rotlimitMin), (cameraRot + rotlimitMax));
+
+            }
+
+            if (Input.GetKey("e"))
+            {
+                rotationY += panSpeed * Time.deltaTime;
+                rotationObject.transform.Rotate(0, rotationY, 0);
+                rotationY = Mathf.Clamp(rotationY, (cameraRot - rotlimitMin), (cameraRot + rotlimitMax));
+
+            }
         }*/
+        
+
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y += scroll * scrollSpeed * 100f * Time.deltaTime;
@@ -65,8 +78,8 @@ public class CameraController : MonoBehaviour
         pos.x = Mathf.Clamp(pos.x, (cameraPos.x - panlimitX), (cameraPos.x + panlimitX));
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
         pos.z = Mathf.Clamp(pos.z, (cameraPos.z - panlimitZ), (cameraPos.z + panlimitZ));
+        //rotationY = Mathf.Clamp(rotationY, (cameraRot - rotlimitMin), (cameraRot + rotlimitMax));
 
-       
         transform.position = pos;
     }
 }
